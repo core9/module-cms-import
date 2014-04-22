444908255b8f6f55fc5f423a00798f658277e084
Mark Wienk <markwienk@gmail.com>
2014-04-22 20:15:12 +0200

    moved

--- src/plugin.js
+++ src/plugin.js
@@ -3,3 +3,5 @@ angular.module( 'core9Dashboard.import', [
   'core9.importer.csv',
   'templates-module-cms-import'
 ]);
+
+angular.module('core9Dashboard.admin.dashboard').requires.push('core9Dashboard.import');
